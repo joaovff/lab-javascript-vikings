7d72d1f8177fd4bf0c02dbe4c1c2f574aafbd8a0
João Silva <joao.figueiredosilva@protonmail.com>
2022-09-25 11:08:49 +0100

    done

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,70 @@
 // Soldier
 class Soldier {
-    constructor(health, strength) {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
+// Viking
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+
+    if (this.health >= 1) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
     }
+  }
+
+  battleCry() {
+    return "Odin Owns You All!";
+  }
 }
 
-// Viking
-class Viking {}
-
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health >= 1) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  vikingArmy = [];
+  saxonArmy = [];
+
+  addViking(viking) {
+    viking = new Viking("viking1", 20, 10); //?
+  }
+
+  addSaxon(saxon) {
+    saxon = new Saxon(10, 20); //?
+  }
+
+  vikingAttack() {
+  }
+
+  saxonAttack() {}
+
+  showStatus() {}  
+
+}
